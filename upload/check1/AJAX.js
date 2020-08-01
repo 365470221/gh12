466beeee507f981791897cebab4d7d1a77c03e3a
“365470221 <365470221@qq.com>
2020-08-01 17:22:42 +0800

    dudu

---
+++ upload/check1/AJAX.js
@@ -0,0 +1,16 @@
+function post(url,param,rollback)
+{
+	var xml=new XMLHttpRequest();
+  xml.onreadystatechange=function()
+  {
+	  if(xml.status==200&&xml.readyState==4)
+		  {
+		  rollback(xml.responseText);
+		  }
+  }
+  xml.open("post",url);
+  
+  xml.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+  
+  xml.send(param);
+}
---
+++ upload/upload/AJAX.js
@@ -0,0 +1,16 @@
+function get(url,param,rollback)
+{
+	var xml=new XMLHttpRequest();
+  xml.onreadystatechange=function()
+  {
+	  if(xml.status==200&&xml.readyState==4)
+		  {
+		  rollback(xml.responseText);
+		  }
+  }
+  xml.open("post",url);
+ if(!(param instanceof FormData)){
+  xml.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+ }
+  xml.send(param);
+}
